4d39a61d12fbdf9c7b606085b55fbe605939f882
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-12-25 16:37:11 -0500

    Add link to register page on signin page

--- website/signin/login.php
+++ website/signin/login.php
@@ -41,6 +41,10 @@ session_start();
       <input type="submit" class="btn btn-primary" name="login" value="Submit">
     </form>
 
+    <div>
+        <p>Don't have an account yet? <a href="register.php">Go to the registration page.</a></p>
+    </div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
