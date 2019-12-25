30d87a49c23e9255f4999475777f83de95d13472
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-12-25 01:30:23 -0500

    Show a banner notifying user on successful logout

--- website/signin/login.php
+++ website/signin/login.php
@@ -25,6 +25,8 @@ session_start();
         echo '<div class="alert alert-danger">Incorrect email or password.</div>';
     else if (@$_GET['status'] == "accountcreated")
         echo '<div class="alert alert-success">Account created.</div>';
+    else if (@$_GET['status'] == "loggedout")
+        echo '<div class="alert alert-success">You have successfully logged out.</div>';
 ?>
     <form class="container" action="../global/crud.php" method="post">
       <div class="form-group">
