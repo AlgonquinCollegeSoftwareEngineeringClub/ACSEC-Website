0621efcf3eec1792c476ac82e7f3f2739941812a
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-12-25 23:58:40 -0500

    Switch to new bootstrap script for all files currently using bootstrap

--- website/register/index.php
+++ website/register/index.php
@@ -1,15 +1,10 @@
-<?php require('registerstatus.php'); ?>
+<?php require 'registerstatus.php'; ?>
+<?php require '../global/bootstrap.php'; ?>
 
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
+    <?php echoBootstrapStyle(); ?>
     <title>Register</title>
   </head>
   <h1>Register</h1>
@@ -45,12 +40,6 @@
         <p>If you're a member of the Software Engineering Club and need a key, message Corvus#4166 on discord.</p>
         <p>Already Have an account? <a href="login.php">Go to the signin page.</a></p>
     </div>
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <?php echoBootstrapScripts(); ?>
   </body>
 </html>
