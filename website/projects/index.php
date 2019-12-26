163109af182d3c13c7cc5a28ee58989bc467995d
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-12-26 01:11:29 -0500

    Create placeholder pages for member and project lists

---
+++ website/projects/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../global/banner.php';
+require '../global/bootstrap.php';
+
+session_start();
+
+?>
+
+<html>
+  <head>
+    <?php echoBootstrapStyle(); ?>
+    <title>Projects</title>
+  </head>
+  <body>
+    <?php echoBanner(); ?>
+    <h1>This page is under construction</h1>
+    <?php echoBootstrapScripts(); ?>
+  </body>
+</html>
