1b73e3207556cc3c8b088ca980f129fb4c60f97b
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-12-27 03:07:57 -0500

    Remove outdated comments

--- website/global/banner.php
+++ website/global/banner.php
@@ -1,22 +1,5 @@
 <?php
 
-// TODO: This file will be used on every navigable page to create a banner at
-// the top that shows two main things.
-
-// At the very top is the user bar.
-
-// If signed in, there will be the user's name, which functions as a link to
-// their member page, and also a sign out button.
-
-// If signed out, there will be a link to the sign in page.
-
-// Under the user bar, there is a navigation bar.
-// The navigation bar will provide access to all the main pages, including:
-// Front Page
-// Challenge List
-// (under construction) Member list
-// (under construction) Project list
-
 function echoBanner($isInParent = false) {
     if ($isInParent)
         $navPrefix = '';
@@ -25,8 +8,6 @@ function echoBanner($isInParent = false) {
 
     // Create User Bar.
     if (isset($_SESSION['MemberId'])) {
-        // TODO: Clicking on the user's name should bring them to their member
-        // page.
         echo '<a href="'. $navPrefix . 'member/?id=' . $_SESSION['MemberId'] . '" class="btn btn-primary">' . $_SESSION['FirstName'] . ' ' . $_SESSION['LastName'] . '</a> ';
         echo '<a href="' . $navPrefix . 'global/signout.php" class="btn">Sign out</a>';
     }
@@ -43,3 +24,5 @@ function echoBanner($isInParent = false) {
     echo '<a href="' . $navPrefix . 'members/" class="btn">Members</a>';
     echo '<hr>';
 }
+
+// EOF
