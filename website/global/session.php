65bb9cf3618cd5a551a09de6ee8f6281f98a45e9
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-12-27 22:00:11 -0500

    Make sign in cookies secure by using authorization tokens

--- website/global/session.php
+++ website/global/session.php
@@ -11,11 +11,24 @@ function startSessionFromCookie() {
         if (isset($_COOKIE["acsec"])) {
             $db = Database::getConnection();
 
-            $query = $db->prepare('SELECT Email, FirstName, LastName FROM Member WHERE MemberId = ?');
-            $query->execute( [$_COOKIE["acsec"]] );
-            $row = $query->fetch();
+            date_default_timezone_set('America/Toronto');
+            $today = date("Y-m-d");
 
-            initSession($_COOKIE["acsec"], $row['Email'], $row['FirstName'], $row['LastName']);
+            $query = $db->prepare('SELECT MemberId, Expire FROM Auth WHERE Token = ?');
+            $query->execute([ $_COOKIE["acsec"] ]);
+            $tokenExistsInDatabase = $query->rowCount() > 0;
+            $data = $query->fetch();
+            $tokenExpired = $today > $data['Expire'];
+            $memberId = $data['MemberId'];
+
+            if ($tokenExistsInDatabase && !$tokenExpired) {
+                $query = $db->prepare('SELECT Email, FirstName, LastName FROM Member WHERE MemberId = ?');
+                $query->execute([ $memberId ]);
+                $row = $query->fetch();
+
+                initSession($_COOKIE["acsec"], $row['Email'], $row['FirstName'], $row['LastName']);
+                createCookie($memberId);
+            }
         }
     }
 }
@@ -28,12 +41,39 @@ function initSession($memberId, $email, $firstName, $lastName) {
 }
 
 function createCookie($memberId) {
+    destroyCookie();
+
+    $token = createToken($memberId);
+
     // Set cookie to expire in ten years.
     $tenYears = 10 * 365 * 24 * 60 * 60;
-    setCookie("acsec", $memberId, time() + $tenYears, '/');
+    setCookie("acsec", $token, time() + $tenYears, '/');
+}
+
+function createToken($memberId) {
+    $token = bin2hex(openssl_random_pseudo_bytes(20));
+
+    date_default_timezone_set('America/Toronto');
+    $expire = date("Y-m-d", strtotime(date("Y-m-d", mktime()) . " + 365 day"));
+
+    $db = Database::getConnection();
+
+    $query = $db->prepare('INSERT INTO Auth(MemberId, Token, Expire) VALUES (?, ?, ?)');
+    $query->execute([ $memberId, $token, $expire ]);
+
+    return $token;
 }
 
 function destroyCookie() {
+    if (isset($_COOKIE['acsec'])) {
+        $db = Database::getConnection();
+
+        $query = $db->prepare('DELETE FROM Auth WHERE Token = ?');
+        $query->execute([ $_COOKIE['acsec'] ]);
+    }
+
     // This will time-out the cookie.
     setcookie("acsec", "", time() - 3600, '/');
 }
+
+// EOF
